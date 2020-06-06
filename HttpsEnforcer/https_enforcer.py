9992ceac6e4adb6c9844e6fc32f5a8bfa4dc1493
Eliran Maman <elirandroid@gmail.com>
2020-06-06 17:04:14 +0300

    Adding The HTTPS Enforcer mechanism

---
+++ HttpsEnforcer/https_enforcer.py
@@ -0,0 +1,33 @@
+import time
+
+from requests import Response
+import requests
+
+class HTTPSEnforcer(object):
+
+    def is_connection_secure(self, request):
+        raise NotImplementedError()
+
+    def enforce(self, request):
+        raise NotImplementedError()
+
+
+class HSTS(HTTPSEnforcer):
+
+    def is_connection_secure(self, request):
+        pass
+
+    def enforce(self, request):
+        """
+        We will generate new response with HSTS header, and send it to the client.
+        :param request: The request
+        :return: None
+        """
+        response = Response()
+        response.status_code = 403
+        response.code = "Forbidden"
+        response._content = ""
+        response.error_type = "Forbidden"
+        response.headers = {'Strict-Transport-Security': "max-age=15552000; preload"}
+        request.send_response(response.status_code)
+        request.send_resp_headers(response)
