d29e50f87885540cf98ade976f0586e306fef140
Royi Hamo <royihamo@gmail.com>
2020-08-21 19:50:55 +0300

    remove validator from init file of Detectors

--- Detectors/__init__.py
+++ Detectors/__init__.py
@@ -9,7 +9,6 @@ from Detectors.brute_force import BruteForce
 from Detectors.bots_detector import Bots as BotsDetector
 from Detectors.proxy_detector import ProxyDetector
 from Detectors.xml_injection_detector import XMLDetector as XMLDetector
-from Detectors.sql_validator import SQLValidator as SQLValidator
 from Detectors.xss_injection_detector import XSSDetector as XSSDetector
 
 
