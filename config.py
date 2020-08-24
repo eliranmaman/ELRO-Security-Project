7fdf91deb4ac65f956ea6041f9fd1574803b81c5
Eliran Maman <elirandroid@gmail.com>
2020-08-24 18:10:42 +0300

    Removing not necessary code.

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from DBAgent.sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy("postgres", "qwerty", "localhost", "5432", "elro_sec")
 db.connect()
-data_path = "../Knowledge_Base"
+data_path = "Knowledge_Base"
 config_path = "{}/config".format(data_path)
 detectors_config_path = "{}/detectors".format(config_path)
 controller_config_path = "{}/controllers".format(config_path)
