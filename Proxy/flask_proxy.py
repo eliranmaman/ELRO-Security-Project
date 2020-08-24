c06bb4b1dafb12b9886341f7e514873179df2226
Eliran Maman <elirandroid@gmail.com>
2020-08-24 20:02:50 +0300

    Code optimization.

--- Proxy/flask_proxy.py
+++ Proxy/flask_proxy.py
@@ -25,51 +25,57 @@ detectors = {
 
 
 def request_handler():
-    log("Start parsing the request", LogLevel.INFO, request_handler)
-    parser = FlaskHTTPRequestParser()
-    parsed_request = parser.parse(request)
-    log("Creating Controller", LogLevel.INFO, request_handler)
-    log("The Controller Detectors are {}".format(detectors), LogLevel.DEBUG, request_handler)
-    controller = ElroController(detectors=detectors)
-    log("Activating controller request handler", LogLevel.INFO, request_handler)
-    response_code, send_to, new_request, parsed_request = controller.request_handler(parsed_request, request)
-    log("Controller response is: {} {}".format(response_code, send_to), LogLevel.DEBUG, request_handler)
-    url = 'https://{}{}?{}'.format(parsed_request.host_name, parsed_request.path, parsed_request.query)
-    if response_code == ControllerResponseCode.NotValid:
-        log("The Request for {} is not valid.".format(request.url), LogLevel.INFO, request_handler)
-        log("Redirecting to {}".format(url), LogLevel.INFO, request_handler)
-        response = Response(status=302, headers={"Location": url})
-    elif response_code == ControllerResponseCode.Valid:
-        send_headers = {key:value for key, value in new_request.headers}
-        log("The Request for {} valid and OK".format(request.url), LogLevel.INFO, request_handler)
-        resp = requests.request(
-            method=parsed_request.method, url=url, verify=True,
-            json=new_request.get_json(), headers=send_headers, params=new_request.args,
-            data=new_request.form
-        )
-        log("The Response is {}".format(to_json(resp)), LogLevel.DEBUG, request_handler)
-        parser = HTTPResponseParser(parsed_request)
-        log("Parse the response", LogLevel.INFO, request_handler)
-        parsed_response = parser.parse(resp)
-        log("Activating controller response handler", LogLevel.INFO, request_handler)
-        response_code, send_to, new_content = controller.response_handler(parsed_response, resp)
+    try:
+        log("Start parsing the request", LogLevel.INFO, request_handler)
+        parser = FlaskHTTPRequestParser()
+        parsed_request = parser.parse(request)
+        log("Creating Controller", LogLevel.INFO, request_handler)
+        log("The Controller Detectors are {}".format(detectors), LogLevel.DEBUG, request_handler)
+        controller = ElroController(detectors=detectors)
+        log("Activating controller request handler", LogLevel.INFO, request_handler)
+        response_code, send_to, new_request, parsed_request = controller.request_handler(parsed_request, request)
         log("Controller response is: {} {}".format(response_code, send_to), LogLevel.DEBUG, request_handler)
+        url = 'https://{}{}?{}'.format(parsed_request.host_name, parsed_request.path, parsed_request.query)
         if response_code == ControllerResponseCode.NotValid:
-            send_content = new_content
+            log("The Request for {} is not valid.".format(request.url), LogLevel.INFO, request_handler)
+            log("Redirecting to {}".format(url), LogLevel.INFO, request_handler)
+            response = Response(status=302, headers={"Location": url})
         elif response_code == ControllerResponseCode.Valid:
-            send_content = resp.content
+            send_headers = {key:value for key, value in new_request.headers}
+            log("The Request for {} valid and OK".format(request.url), LogLevel.INFO, request_handler)
+            resp = requests.request(
+                method=parsed_request.method, url=url, verify=True,
+                json=new_request.get_json(), headers=send_headers, params=new_request.args,
+                data=new_request.form
+            )
+            log("The Response is {}".format(to_json(resp)), LogLevel.DEBUG, request_handler)
+            parser = HTTPResponseParser(parsed_request)
+            log("Parse the response", LogLevel.INFO, request_handler)
+            parsed_response = parser.parse(resp)
+            log("Activating controller response handler", LogLevel.INFO, request_handler)
+            response_code, send_to, new_content = controller.response_handler(parsed_response, resp)
+            log("Controller response is: {} {}".format(response_code, send_to), LogLevel.DEBUG, request_handler)
+            if response_code == ControllerResponseCode.NotValid:
+                send_content = new_content
+            elif response_code == ControllerResponseCode.Valid:
+                send_content = resp.content
+            else:
+                send_content = None
+            excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+            headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+            log("Generating response...", LogLevel.INFO, request_handler)
+            response = Response(status=403) if send_content is None else Response(send_content, resp.status_code, headers)
         else:
-            send_content = None
-        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
-        log("Generating response...", LogLevel.INFO, request_handler)
-        response = Response(status=403) if send_content is None else Response(send_content, resp.status_code, headers)
-    else:
-        log("The Request for {} is not found in the database".format(request.url), LogLevel.INFO, request_handler)
+            log("The Request for {} is not found in the database".format(request.url), LogLevel.INFO, request_handler)
+            response = Response(status=404)
+            abort(404)  # Abort the request.
+        log("Sending response", LogLevel.INFO, request_handler)
+        return response
+    except Exception as e:
         response = Response(status=404)
         abort(404)  # Abort the request.
-    log("Sending response", LogLevel.INFO, request_handler)
-    return response
+        return response
+
 
 
 @app.endpoint('proxy')
