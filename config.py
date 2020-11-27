6984396ab9f66f276f390ac1a1e5a2e0ba20e2f8
Eliran Maman <elirandroid@gmail.com>
2020-11-27 20:07:34 +0200

    Removing keys.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from DBAgent.sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy("postgres", "qwerty", "localhost", "5432", "elro_sec")
+db = SQLAlchemy("postgres", "super secret password", "super secret ip", "5432", "super secret name")
 db.connect()
 data_path = "Knowledge_Base"
 config_path = "{}/config".format(data_path)
@@ -10,4 +10,4 @@ controller_config_path = "{}/controllers".format(config_path)
 
 url_regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
 
-authorized_servers = ["54.162.177.227"]
+authorized_servers = ["super secret ip"]
