7daf8e382226616b212bb7cc69766b5ca14d90fd
Eliran Maman <elirandroid@gmail.com>
2020-11-27 20:07:53 +0200

    reformat file.

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ config_path = "{}/config".format(data_path)
 detectors_config_path = "{}/detectors".format(config_path)
 controller_config_path = "{}/controllers".format(config_path)
 
-
 url_regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
 
 authorized_servers = ["super secret ip"]
